42d0e8d853749df644edf7a575cc8feccbf4351a
ogre <vincent@ogre.be>
2017-02-16 16:55:53 +0100

    debug start script

--- werkingYardUI/start.werkingYard.sh
+++ werkingYardUI/start.werkingYard.sh
@@ -2,5 +2,7 @@ echo "# GO TO /home/pi/werkingYard/werkingYard"
 cd /home/pi/werkingYard/werkingYard
 echo "# git pull werkingYard"
 git pull
-
+echo "# SETUP DISPLAY 0"
+export DISPLAY=:0
+echo "# starting APP"
 processing-java --sketch=/home/pi/werkingYard/werkingYard/werkingYardUI/GRemote --present
