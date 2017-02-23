6d495c4939ba5bae073111dbadb7813475bebfd1
oogreOnPI <vincent@ogre.be>
2017-02-23 10:43:30 +0000

    ignore

--- werkingYardUI/start.werkingYard.sh
+++ werkingYardUI/start.werkingYard.sh
@@ -1,14 +1,15 @@
-#echo "# GO TO /home/pi/werkingYard/werkingYard"
-#cd /home/pi/werkingYard/werkingYard
-#echo "# git pull werkingYard"
-#git pull
-#echo "# SETUP DISPLAY 0"
-#export DISPLAY=:0
-#echo "# starting APP"
-#processing-java --sketch=/home/pi/werkingYard/werkingYard/werkingYardUI/GRemote --present
-
-
-cd /Users/ogre/Work/7102/felix/WerkingYard
+echo "# GO TO /home/pi/werkingYard/werkingYard"
+cd /home/pi/werkingYard/werkingYard
+echo "# git pull werkingYard"
 git pull
 cd werkingYardUI
-sudo processing-java --sketch=/Users/ogre/Work/7102/felix/WerkingYard/werkingYardUI/GRemote --run ${PWD}
+echo "# SETUP DISPLAY 0"
+export DISPLAY=:0
+echo "# starting APP"
+sudo processing-java --sketch=/home/pi/werkingYard/werkingYard/werkingYardUI/GRemote --present ${PWD}
+
+
+#cd /Users/ogre/Work/7102/felix/WerkingYard
+#git pull
+#cd werkingYardUI
+#sudo processing-java --sketch=/Users/ogre/Work/7102/felix/WerkingYard/werkingYardUI/GRemote --run ${PWD}
